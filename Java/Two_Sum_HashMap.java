bb56e92c71dcac73f1c41dbda2190d99a7bad985
ok4mi33 <uchiha.d.dash@gmail.com>
2026-08-06 12:21:31 -0500

    Two Sum en Java - HashMap O(n), con notas de complemento y duplicados

--- Java/Two_Sum_HashMap.java
+++ Java/Two_Sum_HashMap.java
@@ -7,8 +7,38 @@ import java.util.HashMap; // declaracion: HashMap<TipoDeClave, TipoDeValor> nomb
 
 public class Two_Sum_HashMap {
 	public static void main(String[] args) {
-		// defino el HashMap
-		int HashMap<i, j> nums = new HashMap<>();
+		
+    // defino el array de numeros
+    int[] nums = {3, 4, 3, 4 ,5, 6};
+
+    // defino el target
+    int target = 9;
+    
+    // declaro el HashMap vacio (clave = numer, valor = indice)
+    HashMap<Integer, Integer> recorrido = new HashMap<>();
+    
+    // recorro el array una sola vez (loop)
+    for(int i = 0; i < nums.length; i++){
+    
+          // calculo el complemento (target - numero actual)
+          int complemento = target - nums[i];
+          
+          // pregunto si el complemento ya esta guardado en el HashMap
+          if(recorrido.containsKey(complemento)){
+          //
+                  // si esta, imprimo el indice guardado (con .get) y el indice actual
+                  System.out.println(recorrido.get(complemento) + "," + i);
+                  
+          // si no esta, guardo el numero actual con su indice en el HashMap
+          } else { 
+          recorrido.put(nums[i], i);
+          
+          
+      }
+    }
+  }
+}
+
 
 
 
