9339d73a340289e4267d0d66347f8677a8e82bed
ok4mi33 <uchiha.d.dash@gmail.com>
2026-09-02 15:25:21 -0500

    WIP: Average of Leves in Binary Tree - logica en palabras y sintaxis rapasada, falta escribir el codigo

---
+++ Java/AverageLevelsBinaryTree.java
@@ -0,0 +1,41 @@
+import java.util.Queue
+
+public class AverageLevesBinaryTree {
+
+  public static class TreeNode {
+
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int val) {
+        this.val = val;
+      }
+  }
+
+  public class static ArrayList(TreeNode node) {
+
+    // meto la raiz en la cola
+
+
+    //mientras haya elementos en la cola:
+    // cantidadEnNivel = cola.size()
+    // sumaNivel = 0
+    //
+    //
+    // repito "cantidadEnNivel" veces:
+    // saco un nodo de la cola (poll)
+    // le sumo a sumaNivel el valor del nodo que acabo de sacar
+    // si el nodo tiene hijo izquierdo, lo meto a la cola
+    // 
+    //
+    // promedio = sumaNivel / cantidadEnNivel (como decimal)
+    // agrego ese promedio a la lista de resultados
+    //
+    // devuelvo la lista de resultados
+
+
+
+  }
+
+
+}
